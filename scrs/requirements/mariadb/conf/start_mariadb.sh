03112752a13cb94f83839a0214b3c5ed3188c6f0
ldalmass <ldalmass@ldalmass.ldalmass.42.fr>
2025-03-03 23:39:34 +0100

    mariadb almost done ?

--- scrs/requirements/mariadb/conf/start_mariadb.sh
+++ scrs/requirements/mariadb/conf/start_mariadb.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
+rc-service mariadb start;
 
+if [ ! -d "/var/lib/mysql" ];
+then
+    RUN mkdir -p /var/lib/mysql
+    chown -R mysql:mysql /var/lib/mysql
+
+    # Initialize the MariaDB database
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
+# from http://tuto.grademe.fr/inception/#mariadb
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
+
+# Start mariadb
 exec su-exec mysql mariadbd --datadir=/var/lib/mysql --user=mysql
