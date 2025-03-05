bb0a1bec93d4d7684423993469b44a389a635377
MazeWave <louisdalmasso2003@gmail.com>
2025-03-05 20:45:41 +0100

    removed entrypoint.sh for the nginx

--- srcs/requirements/nginx/conf/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Create needed folders if not here
-mkdir -p /var/run/nginx /var/log/nginx
-chown -R nginx:nginx /var/run/nginx /var/log/nginx
-
-# start nginx in foreground
-exec nginx -g 'daemon off;'
